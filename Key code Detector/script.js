11026a791e91437c3400ae5e0b681fd56cfedacd
Abhishek120797 <abhi.jais1211@gmail.com>
2024-01-24 18:31:58 +0530

    uploaded

---
+++ Key code Detector/script.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById("insert");
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `<div class="table">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Key code</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${event.key}</td>
+                <td>${event.keyCode}</td>
+                <td>${event.code}</td>
+            </tr>
+            
+        </table>
+    </div>`;
+});
