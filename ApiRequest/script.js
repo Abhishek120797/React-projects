3f8734f8bbe2834a2c6ce5ac19dfe32d35cff953
Abhishek120797 <abhi.jais1211@gmail.com>
2024-01-24 23:53:38 +0530

    "uploaded"

---
+++ ApiRequest/script.js
@@ -0,0 +1,22 @@
+let cardImage = document.querySelector(".card img");
+let name = document.querySelector(".details h2");
+let followers = document.querySelector(".details h3");
+const button = document.querySelector("#getDetails");
+
+const requrl = "https://api.github.com/users/hiteshChoudhary";
+
+const xhr = new XMLHttpRequest();
+
+button.addEventListener("click", () => {
+  xhr.open("GET", requrl);
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      const data = JSON.parse(this.responseText);
+      cardImage.setAttribute("src", data.avatar_url);
+      name.innerText = data.name;
+      followers.innerText = `Followers : ${data.followers}`;
+    }
+  };
+});
