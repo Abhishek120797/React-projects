073532ed8fbe235e4fcc3b905b9df1fb1a986a83
Abhishek120797 <abhi.jais1211@gmail.com>
2024-02-15 18:24:52 +0530

    updated

---
+++ React/MegaBlogWithAppWrite/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>A blog with appWrite</h1>
+    </>
+  )
+}
+
+export default App
--- React/TailwindProps-react/tailwindprops/src/App.jsx
+++ React/TailwindProps-react/tailwindprops/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Card from './components/Card'
 
@@ -12,10 +11,10 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      
+    <div className='flex justify-evenly'>
     <Card  userName="Anchal Jaiswal" btnText="click me" />
     <Card  userName="Sunita Jaiswal" btnText="Visit Me" />
-
+    </div>
     </>
   )
 }
