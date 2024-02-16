f3af48c117237787bd9a631ad645cd527965c55c
Abhishek120797 <abhi.jais1211@gmail.com>
2024-02-16 12:42:15 +0530

    updated

--- React/MegaBlogWithAppWrite/src/App.jsx
+++ React/MegaBlogWithAppWrite/src/App.jsx
@@ -1,13 +1,41 @@
 
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import { logIn, logOut } from './store/authSlice'
+import {Header, Footer} from './components'
 
 function App() {
- 
-  return (
-    <>
-      <h1>A blog with appWrite</h1>
-    </>
-  )
+
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(logIn({userData}))
+      }else{
+        dispatch(logOut())
+      }
+    })                                   //write catch block
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* todo: <Outlet/> */}    
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null;    //improve false condition
 }
 
 export default App
---
+++ React/MegaBlogWithAppWrite/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import Container from "../components/container/Container";
+import LogOutBtn from "./header/LogOutBtn";
+
+export { Header, Footer, Logo, Container, LogOutBtn };
