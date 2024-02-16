5ad18b306888e64624736d667d27ded7e6f629fe
Abhishek120797 <abhi.jais1211@gmail.com>
2024-02-16 20:21:45 +0530

    updated

--- React/MegaBlogWithAppWrite/src/components/index.js
+++ React/MegaBlogWithAppWrite/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Container from "../components/container/Container";
 import LogOutBtn from "./header/LogOutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, Container, LogOutBtn };
+export { Header, Footer, Logo, Container, LogOutBtn, RTE };
